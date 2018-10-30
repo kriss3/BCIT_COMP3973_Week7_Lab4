b5a1eeffb58448bf4c78a20223aea032886c7f7a
Kris <kris_s2@outlook.com>
2018-10-30 01:34:53 -0700

    Adding button to each row for deltion; still need to figure out how to attach event to call a function to each button;

--- CorsClient/wwwroot/script/partyScript.js
+++ CorsClient/wwwroot/script/partyScript.js
@@ -1,57 +1,5 @@
 ﻿$(function () {
-	var getParties = function () {
-		var url = "https://localhost:44343/api/parties";
-		$.get(url).always(showResponse);
-		return false;
-	};
 
-	var showResponse = function (object) {
-		$("#preOutput").text(JSON.stringify(object, null, 4));
-	};
-
-	$("#btnGetParties").click(getParties);
-
-	var getAlert = function myfunction() {
-		$("#myForm").text("Response from my js script");
-	};
-
-	$("#btnGetAlert").click(getAlert);
-
-	//POST new Patient/Ailments/Mediction to backend API
-	/*
-	$("#postform").submit(function (e) {
-		e.preventDefault();
-		var url = "https://localhost:44343/api/patients";
-		var data = {
-			name: $("#txtPatientName").val().trim(),
-			ailments: [
-				{ name: $("#txtAilmentName1").val().trim() }
-				//{ name: $("#txtAilmentName2").val().trim()}
-			],
-			medications: [
-				{ name: $("#txtMedication1").val().trim(), doses: $("#txtDose1").val().trim() }
-				//{ name: $("#txtMedication2").val().trim(), doses: $("#txtDose2").val().trim() }
-			]
-		};
-
-		$.ajax({
-			contentType: 'application/json',
-			type: 'POST',
-			url: url,
-			data: JSON.stringify(data),
-			success: function (d) {
-				alert("Saved Successfully");
-				$("#postResult").text("Record created ok. Id=" + d.patientId);
-				$("#postform").trigger("reset");
-			},
-			error: function (d) {
-				alert("Error please try again: " + JSON.stringify(d));
-			}
-		});
-
-	});
-	//end 
-	*/
 	$("#btnGetPartiesTbl").click(getPartiesForTbl);
 
 	//GET ALL Parties from backend API
@@ -65,13 +13,19 @@
 				var party = '';
 				$('#party_table tbody').empty();
 				$.each(data, function (key, value) {
+
+					
+					
 					party += '<tr>';
 					party += '<td>' + value.partyName + '</td>';
 					party += '<td>' + getMyDate(value.partyDate); + '</td>';
 					party += '<td>' + value.expectedNumberOfGuests + '</td>';
 					party += '<td>' + value.location + '</td>';
+					party += '<td>' + '<button class="btn btn-default" id="btnDel">Delete ('+value.partyId+')</button>' +'</td>';
 					party += '</tr>';
+					
 				});
+				
 				$('#party_table').append(party);
 			},
 			error: function (data) {
@@ -82,7 +36,6 @@
 	//end
 
 	//POST NEW Party to the API
-	//Begin
 	$("#postform").submit(function (e) {
 		e.preventDefault();
 		var url = "https://localhost:44343/api/parties";
@@ -120,4 +73,6 @@
 
 		return day + '/' + month + '/' + year;
 	}
+
+
 });
